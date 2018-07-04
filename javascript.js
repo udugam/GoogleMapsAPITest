184f6c2a7451d738a99c2903f804600133bba442
udugam <ryan.udugam@gmail.com>
2018-07-03 23:52:30 -0400

    setup up firebase and succesfully storing coordinates of clicked locations

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCDSfVNfZX6SPPlwLjOrW4YJUzXNoKR4LI",
+    authDomain: "mapsapitest-f1b56.firebaseapp.com",
+    databaseURL: "https://mapsapitest-f1b56.firebaseio.com",
+    projectId: "mapsapitest-f1b56",
+    storageBucket: "mapsapitest-f1b56.appspot.com",
+    messagingSenderId: "703244107069"
+};
+
+firebase.initializeApp(config);
+
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+
+
 // Initialize and add the map
 function initMap() {
     // The location of Toronto
@@ -9,8 +25,21 @@ function initMap() {
     // var marker = new google.maps.Marker({position: toronto, map: map});
 
     map.addListener('click', function(event) {
-        $("#lat").text("Latitude: "+event.latLng.lat());
-        $("#long").text("Longitude: "+event.latLng.lng());
+        //Store lat and long in local variables
+        var lat = event.latLng.lat()
+        var long = event.latLng.lng()
+
+        //Display lat and long on html page
+        $("#lat").text("Latitude: "+lat);
+        $("#long").text("Longitude: "+long);
+
+        //Store lat and long in database
+        database.ref().push({
+            latitude: lat,
+            longitude: long
+        })
     })
 }
+
+
 
