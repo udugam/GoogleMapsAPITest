d2d1de31898611f5268fbffc434ad8b6dd402da9
Charles <charleshu32@gmail.com>
2018-07-11 20:58:07 -0400

    Revert "removed click listener on map that adds markers, added distance calc …"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,27 @@ function initMap() {
         document.getElementById('map'), { zoom: 10, center: toronto });
     // The marker, positioned at Toronto
 
+    map.addListener('click', function (event) {
+        //Store lat and long in local variables
+        lat = event.latLng.lat()
+        long = event.latLng.lng()
+
+        //Display lat and long on html page
+
+        //Add marker to map
+        var marker = new google.maps.Marker({
+            position: { lat: lat, lng: long },
+            map: map,
+            title: "POI"
+        })
+
+        //Store lat and long in database
+        // database.ref().push({
+        //     latitude: lat,
+        //     longitude: long
+        // })
+    })
+
     //Setup listener for everytime a child is added to the root for the database
     database.ref().on("child_added", function (snapshot) {
         console.log(snapshot.val());
@@ -213,33 +234,12 @@ function getLocation() {
 			var marker = new google.maps.Marker({
 				position: { lat: lat, lng: long },
 				map: map,
-				title: "myLocation",
-				icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+				title: "POI"
 			})
 			
 		}, function() {
 			//handleLocationError(true, infoWindow, map.getCenter());
 		});
-	}
+	} 
 }
-
-//This function accepts two coordinate objects and returns the distance between them
-function distance(lat1, lon1, lat2, lon2, unit) {
-
-	var radlat1 = Math.PI * lat1/180
-	var radlat2 = Math.PI * lat2/180
-	var theta = lon1-lon2
-	var radtheta = Math.PI * theta/180
-	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	if (dist > 1) {
-		dist = 1;
-	}
-	dist = Math.acos(dist)
-	dist = dist * 180/Math.PI
-	dist = dist * 60 * 1.1515
-	if (unit=="K") { dist = dist * 1.609344 }
-	if (unit=="N") { dist = dist * 0.8684 }
-	return dist
-}
-
-
+	
