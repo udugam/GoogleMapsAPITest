0067aa3a74a02f32a5ad64e66e8a5026c5b1e6e5
Niuniu32 <charleshu32@gmail.com>
2018-07-14 11:47:42 -0400

    Updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 var config = {
-	apiKey: 'AIzaSyCDSfVNfZX6SPPlwLjOrW4YJUzXNoKR4LI',
-	authDomain: 'mapsapitest-f1b56.firebaseapp.com',
-	databaseURL: 'https://mapsapitest-f1b56.firebaseio.com',
-	projectId: 'mapsapitest-f1b56',
-	storageBucket: 'mapsapitest-f1b56.appspot.com',
-	messagingSenderId: '703244107069'
+    apiKey: 'AIzaSyCDSfVNfZX6SPPlwLjOrW4YJUzXNoKR4LI',
+    authDomain: 'mapsapitest-f1b56.firebaseapp.com',
+    databaseURL: 'https://mapsapitest-f1b56.firebaseio.com',
+    projectId: 'mapsapitest-f1b56',
+    storageBucket: 'mapsapitest-f1b56.appspot.com',
+    messagingSenderId: '703244107069'
 };
 
 var API_KEY2 = 'AIzaSyDjQn6mu7DMp57MahmCyoS334lFoNXlzmE'
@@ -20,13 +20,15 @@ var map;
 
 
 function initMap() {
-  
+
     // The location of Toronto
     var toronto = { lat: 43.70011, lng: -79.4163 };
     // The map, centered at Uluru
     map = new google.maps.Map(
         document.getElementById('map'), { zoom: 10, center: toronto });
     // The marker, positioned at Toronto
+
+    getLocation();
 
     //Setup listener for everytime a child is added to the root for the database
     database.ref().on("child_added", function (snapshot) {
@@ -42,112 +44,91 @@ function initMap() {
 
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
-	});
-	
+    });
+
 }
 $.fn.extend({
-	animateCss: function(animationName, callback) {
-		var animationEnd = (function(el) {
-			var animations = {
-				animation: 'animationend',
-				OAnimation: 'oAnimationEnd',
-				MozAnimation: 'mozAnimationEnd',
-				WebkitAnimation: 'webkitAnimationEnd',
-			};
+    animateCss: function (animationName, callback) {
+        var animationEnd = (function (el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
 
-			for (var t in animations) {
-				if (el.style[t] !== undefined) {
-					return animations[t];
-				}
-			}
-		})(document.createElement('div'));
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
 
-		this.addClass('animated ' + animationName).one(animationEnd, function() {
-			$(this).removeClass('animated ' + animationName);
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
 
-			if (typeof callback === 'function') callback();
-		});
+            if (typeof callback === 'function') callback();
+        });
 
-		return this;
-	},
+        return this;
+    },
 });
 
 $(document).ready(function () {
-	$('._AddLocForm').hide();
+    $('._AddLocForm').hide();
     $('#_alertMessage').hide();
-	$('._searchResults').hide();
+    $('._searchResults').hide();
     $('.action-buttons').hide();
-	$('._addPlace').on('click', function () {
-		$('._header').animateCss('bounceOutUp', function() {
-			$('._header').removeClass('bounceOutUp').hide();
-		});
-		$('._action-buttons').animateCss('bounceOutUp', function() {
-			$('._action-buttons').removeClass('bounceOutUp').hide();
-		});
-		$('#map').animate({
-			height: "80vh"
-		}, 2000, function() {
-			// Animation complete.
-		});
+    $('._addPlace').on('click', function () {
+        $('._header').animateCss('bounceOutUp', function () {
+            $('._header').removeClass('bounceOutUp').hide();
+        });
+        $('._action-buttons').animateCss('bounceOutUp', function () {
+            $('._action-buttons').removeClass('bounceOutUp').hide();
+        });
+        $('#map').animate({
+            height: "80vh"
+        }, 2000, function () {
+            // Animation complete.
+        });
         $('#_locForm').show();
         $('#_alertMessage').hide();
-		$('._AddLocForm').show().delay(500).addClass('animated flipInY').removeClass('flipInY');
-		$('.action-buttons').show().delay(500).addClass('animated flipInY').removeClass('flipInY');
+        $('._AddLocForm').show().delay(500).addClass('animated flipInY').removeClass('flipInY');
+        $('.action-buttons').show().delay(500).addClass('animated flipInY').removeClass('flipInY');
         $('._searchResults').hide();
-	});
+    });
 
-	$('#cancelLocation').on('click', function () {
-		$('._header').show().delay(500).addClass('animated bounceInDown').removeClass('bounceInDown');
-		$('._action-buttons').show().delay(500).addClass('animated bounceInDown').removeClass('bounceInDown');
-		$('#map').animate({
-			height: "60vh"
-		}, 2000, function() {
+    $('#cancelLocation').on('click', function () {
+        $('._header').show().delay(500).addClass('animated bounceInDown').removeClass('bounceInDown');
+        $('._action-buttons').show().delay(500).addClass('animated bounceInDown').removeClass('bounceInDown');
+        $('#map').animate({
+            height: "60vh"
+        }, 2000, function () {
 
-		});
-		$('._AddLocForm').addClass('animated slideInUp').delay(500).hide('slow').removeClass('slideInUp');
-		$('.action-buttons').addClass('animated slideInUp').delay(500).hide('slow').removeClass('slideInUp');
+        });
+        $('._AddLocForm').addClass('animated slideInUp').delay(500).hide('slow').removeClass('slideInUp');
+        $('.action-buttons').addClass('animated slideInUp').delay(500).hide('slow').removeClass('slideInUp');
         $('._searchResults').hide();
-	});
+    });
 });
 
 
-document.getElementById("submitLocation").addEventListener("click",function(){
+document.getElementById("submitLocation").addEventListener("click", function () {
     event.preventDefault();
     console.log("submit");
-    var name=document.getElementById("nameInput").value.trim();
+    var name = document.getElementById("nameInput").value.trim();
     console.log(name);
-    var description=document.getElementById("addLocDesc").value.trim();
+    var description = document.getElementById("addLocDesc").value.trim();
     console.log(description);
-
-    if(name && description) {
-		database.ref().push({
-			nameID: name,
-			latitude: lat,
-			longitude: long,
-			city: city,
-			desc: description,
-			descSearch: description.toLowerCase()
-		});
-        $('#_alertMessage').show();
-        $('#_locForm').hide().reset();
-    } else {
-    	alert('Please enter the location name and description.');
-	}
-    
-});
-
-$('._addPlace').on("click", function() {
-	getLocation();
-});
     if (validation(name, description, lat, long) === "condition_pass") {
-        database.ref().push({
-			nameID: name,
-			latitude: lat,
-			longitude: long,
-			city: city,
-			desc: description,
-			descSearch: description.toLowerCase()
-		});
+        city = city.toLowerCase();
+        database.ref("/City/" + city).push({
+            nameID: name,
+            latitude: lat,
+            longitude: long,
+            desc: description,
+            descSearch: description.toLowerCase()
+        });
     }
     else if (validation(name, description, lat, long) === "condition_no_lat&long") {
         alert("Plz Get Lat and Long");
@@ -161,21 +142,21 @@ $('._addPlace').on("click", function() {
     else if (validation(name, description, lat, long) === "YOUSHALLNOTPASS") {
         alert("Plz type in something");
     }
-   
-    
+
+
 })
-$('._addPlace').on("click", function() {
-	getLocation();
+$('._addPlace').on("click", function () {
+    getLocation();
 })
 function updateDescription() {
-    var descritpion=document.getElementById("addLocDesc").value;
+    var descritpion = document.getElementById("addLocDesc").value;
     return descritpion;
 }
 //Check if there is anything in the add your location
 function validation(name, description, lat, long) {
     if (name == "" || description == "" || typeof lat == "undefined" && typeof long == "undefined") {
         console.log("Nothing");
-        if (name != "" && description != ""&&typeof lat == "undefined" && typeof long == "undefined") {
+        if (name != "" && description != "" && typeof lat == "undefined" && typeof long == "undefined") {
             console.log("Type In long lat");
             return "condition_no_lat&long";
         }
@@ -194,102 +175,117 @@ function validation(name, description, lat, long) {
         return "condition_pass";
 }
 
-function searchItem(searchName){
+// function searchItem(searchName) {
+//     var foundLocations = [];
+//     var foundCity=[];
+//     city=city.toLowerCase();
+//     database.ref("/City/"+city).orderByChild("descSearch").equalTo(searchName).on("child_added", function (snapshot) {
+//         var item = snapshot.val().descSearch;
 
-	var foundLocations = [];
-    database.ref().orderByChild("descSearch").startAt(searchName).endAt(searchName+"\u{F8FF}").on("child_added",function(snapshot){
-       var item=snapshot.val();
-       foundLocations.push(item);
-    });
-    if(foundLocations.length > 0) {
-        jQuery('#findPlaceModal').modal('hide');
-        renderResults(foundLocations);
-    } else {
-    	alert('No locations found. Please try again.');
-	}
-}
+//         console.log(item);
+//         foundLocations.push(item);
+//     });
+//     if (foundLocations.length > 0) {
+//         jQuery('#findPlaceModal').modal('hide');
+//         renderResults(foundLocations);
+//     } else {
+//         alert('No locations found. Please try again.');
+//     }
+// }
 
 
 // render table with results
 function renderResults(resultsArray) {
     $("#searchResultsTable").empty();
-    resultsArray.forEach(function (element, index) {
-        $("#searchResultsTable").append("<tr><td>" + element.city + "</td><td>" + element.desc + "</td>")
-    });
+    for (var i = 0; i < resultsArray.length; i++) {
+        $("#searchResultsTable").append("<tr><td>" + city + "</td><td>" + resultsArray[i] + "</td>")
+    };
     $('._searchResults').show('slow');
 }
 
-  function searchCity(City){
-    database.ref("/City").orderByChild(searchName).on("value",function(snapshot){
-       var description=snapshot.val();
-       console.log(description);
-    })
+function searchItem(radius) {
+    city = city.toLowerCase();
+    var foundLocations = [];
+    database.ref("/City/" + city).on("child_added", function (snapshot) {
+        if (radius === distance(lat, long, snapshot.val().latitude, snapshot.val().longitude, m)) {
+            var description = snapshot.val().desc;
+            console.log(description); 
+            foundLocations.push(description);
+        }
+
+    });
+    if (foundLocations.length > 0) {
+        jQuery('#findPlaceModal').modal('hide');
+        renderResults(foundLocations);
+    } else {
+        alert('No locations found. Please try again.');
+    }
 
 }
 
 
 
 //Function for Search button
-document.getElementById("submitSearch").addEventListener("click",function(){
-    var searchName=document.getElementById("addDescription").value.toLowerCase();
+document.getElementById("submitSearch").addEventListener("click", function () {
+    var searchName = document.getElementById("addDescription").value.toLowerCase();
     console.log(searchName);
     searchItem(searchName);
 
 });
 
 function getLocation() {
-	//These statements 
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			lat= position.coords.latitude;
-			long= position.coords.longitude;
-			getCity(lat,long);
+    //These statements 
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            getCity(lat, long);
 
-			var center = {lat: lat, lng:long}
-			map.setOptions({ zoom: 13, center: center });
-			
+            var center = { lat: lat, lng: long }
+            map.setOptions({ zoom: 13, center: center });
 
-			var marker = new google.maps.Marker({
-				position: { lat: lat, lng: long },
-				map: map,
-				title: "myLocation",
-				icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-			})
-			
-		}, function() {
-			//handleLocationError(true, infoWindow, map.getCenter());
-		});
-	}
+
+            var marker = new google.maps.Marker({
+                position: { lat: lat, lng: long },
+                map: map,
+                title: "myLocation",
+                icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+            })
+
+        }, function () {
+            //handleLocationError(true, infoWindow, map.getCenter());
+        });
+    }
 }
 
 //This function accepts two coordinate objects and returns the distance between them
 function distance(lat1, lon1, lat2, lon2, unit) {
 
-	var radlat1 = Math.PI * lat1/180;
-	var radlat2 = Math.PI * lat2/180;
-	var theta = lon1-lon2
-	var radtheta = Math.PI * theta/180;
-	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	if (dist > 1) {
-		dist = 1;
-	}
-	dist = Math.acos(dist);
-	dist = dist * 180/Math.PI;
-	dist = dist * 60 * 1.1515;
-	if (unit=="K") { dist = dist * 1.609344 }
-	if (unit=="N") { dist = dist * 0.8684 }
-	return dist
+    var radlat1 = Math.PI * lat1 / 180;
+    var radlat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2
+    var radtheta = Math.PI * theta / 180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+        dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
+    return dist
 }
 
 //This function return the city of a given lat, and long
 function getCity(lat, long) {
-    var queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&result_type=locality&key="+API_KEY2
-	var regionResponse;
+    var queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&result_type=locality&key=" + API_KEY2
+    var regionResponse;
 
-	$.ajax({url:queryUrl,method:"GET"}).then(function(response) {
-		regionResponse = response.results[0].formatted_address
-		
-		city = regionResponse.slice(0,regionResponse.indexOf(','))
+    $.ajax({ url: queryUrl, method: "GET" }).then(function (response) {
+        regionResponse = response.results[0].formatted_address
+
+        city = regionResponse.slice(0, regionResponse.indexOf(','))
     })
 }
 
